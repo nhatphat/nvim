1b1e6fdc5c131865f7cd0bf63d469ad8d4970e49
nhatphat <phatle@teqnological.asia>
2025-12-02 10:08:22 +0700

    custom snacks plugin

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,57 @@
+local exclude_patterns = {
+  -- Git
+  ".git",
+  -- JavaScript/Node
+  "node_modules",
+  -- Python
+  ".venv",
+  "venv",
+  "__pycache__",
+  ".pytest_cache",
+  ".mypy_cache",
+  ".ruff_cache",
+  ".tox",
+  "*.egg-info",
+  ".eggs",
+  ".ipynb_checkpoints",
+  "htmlcov",
+  ".hypothesis",
+  -- IDE/Editor
+  ".idea",
+  ".vscode",
+  ".DS_Store",
+}
+
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+        },
+        files = {
+          hidden = true,
+          ignored = true,
+          exclude = exclude_patterns,
+        },
+        grep = {
+          hidden = true,
+          ignored = true,
+          exclude = exclude_patterns,
+        },
+        grep_word = {
+          hidden = true,
+          ignored = true,
+          exclude = exclude_patterns,
+        },
+        grep_buffers = {
+          hidden = true,
+          ignored = true,
+          exclude = exclude_patterns,
+        },
+      },
+    },
+  },
+}
