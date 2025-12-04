d79cfe724189445a25c8057bf7239384e885528b
nhatphat <phatle@teqnological.asia>
2025-12-04 15:43:14 +0700

    custom dashboard session

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -12,6 +12,12 @@ return {
 ╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝       ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   
                                                                         ]],
       },
+      sections = {
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
     },
   },
 }
