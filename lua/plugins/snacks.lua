4fc9e2770d39b853146b12616eabf6001da18775
nhatphat <phatle@teqnological.asia>
2025-12-10 16:16:13 +0700

    hide gitignore by default in explorer

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -29,7 +29,7 @@ return {
       sources = {
         explorer = {
           hidden = true,
-          ignored = true,
+          -- ignored = true,
         },
         files = {
           hidden = true,
