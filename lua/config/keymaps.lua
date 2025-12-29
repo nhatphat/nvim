77e115d2bd34edae3f630646b41cd7e562447520
phatle-teq <78527396+phatle-teq@users.noreply.github.com>
2025-12-29 16:46:47 +0700

    map \ to ctrl+o and map ctrl+/ to <leader>fT

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,3 +11,23 @@ vim.keymap.set("i", "<C-,>", "<Esc>gcca", { remap = true, silent = true, desc = 
 vim.keymap.set("n", "<D-s>", "<cmd>wall<CR>", { silent = true, desc = "Save all files" })
 vim.keymap.set("i", "<D-s>", "<Esc><cmd>wall<CR>a", { silent = true, desc = "Save all files" })
 vim.keymap.set("v", "<D-s>", "<Esc><cmd>wall<CR>gv", { silent = true, desc = "Save all files" })
+
+-- map \ to behave like Ctrl+o (jump to previous location)
+vim.keymap.set("n", "\\", "<C-o>", { silent = true, desc = "Jump to previous location" })
+
+-- Terminal keymaps - Ctrl+/ to toggle terminal at cwd (like LazyVim's <leader>fT)
+vim.keymap.set("n", "<C-/>", function()
+  Snacks.terminal(nil, { cwd = vim.fn.getcwd() })
+end, { desc = "Terminal (cwd)" })
+
+vim.keymap.set("i", "<C-/>", function()
+  vim.cmd("stopinsert")
+  Snacks.terminal(nil, { cwd = vim.fn.getcwd() })
+end, { desc = "Terminal (cwd)" })
+
+vim.keymap.set("v", "<C-/>", function()
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
+  Snacks.terminal(nil, { cwd = vim.fn.getcwd() })
+end, { desc = "Terminal (cwd)" })
+
+vim.keymap.set("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
