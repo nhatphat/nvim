c8a8ab514a33d47081694b38e212db65a080ff67
phatle-teq <78527396+phatle-teq@users.noreply.github.com>
2026-01-06 06:35:14 +0700

    turn off terminal floating

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -53,12 +53,12 @@ return {
         },
       },
     },
-    terminal = {
-      win = {
-        position = "float",
-        backdrop = false,
-        border = "rounded",
-      },
-    },
+    -- terminal = {
+    --   win = {
+    --     position = "float",
+    --     backdrop = false,
+    --     border = "rounded",
+    --   },
+    -- },
   },
 }
