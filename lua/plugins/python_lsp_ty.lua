7cd5e8af3e35837528257adaf207193b715ca0f7
phatle-teq <78527396+phatle-teq@users.noreply.github.com>
2026-01-07 16:11:21 +0700

    update config for ty lsp

--- lua/plugins/python_lsp_ty.lua
+++ lua/plugins/python_lsp_ty.lua
@@ -5,30 +5,20 @@ return {
       vim.g.lazyvim_python_lsp = "ty"
       vim.g.lazyvim_python_ruff = "ruff"
 
-      -- Auto detect .venv
-      local function get_python_path(workspace)
-        if vim.fn.isdirectory(workspace .. "/.venv") == 1 then
-          return workspace .. "/.venv/bin/python"
-        elseif vim.fn.isdirectory(workspace .. "/venv") == 1 then
-          return workspace .. "/venv/bin/python"
-        else
-          return vim.fn.exepath("python3")
-        end
-      end
-
       opts.servers = opts.servers or {}
-      opts.servers.pyright = nil
 
       opts.servers.ty = {
         settings = {
           ty = {
             diagnosticMode = "openFilesOnly",
             configuration = {
-              python = get_python_path(vim.fn.getcwd()),
-
               rules = {
                 ["unused-ignore-comment"] = "warn",
               },
+            },
+            inlayHints = {
+              variableTypes = false,
+              callArgumentNames = false,
             },
           },
         },
