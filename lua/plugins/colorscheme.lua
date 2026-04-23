6a55b1cdab775bcf40dc5c1cbff086d554441e0b
nhatphat <npath2@gmail.com>
2026-04-23 21:28:32 +0700

    add helm-ls

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,4 +9,22 @@ return {
       colorscheme = "gruvbox",
     },
   },
+  -- {
+  --   "sainnhe/everforest",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     -- Optionally configure and load the colorscheme
+  --     -- directly inside the plugin declaration.
+  --     vim.g.everforest_enable_italic = true
+  --     vim.cmd.colorscheme("everforest")
+  --   end,
+  -- },
+  -- { "EdenEast/nightfox.nvim" },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "dayfox",
+  --   },
+  -- },
 }
